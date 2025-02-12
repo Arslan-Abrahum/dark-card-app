212b5acb1738781cd7a091fa16b7e9fded26dac2
Arslan  Ibrahim <abrahum9328@gmail.com>
2025-02-12 19:28:28 +0500

    Added New update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
-    const [username, setUsername] = useState("Sohail302")
-    const [password, setPassword] = useState("Sohail302")
+    const [username, setUsername] = useState("Sohail445566")
+    const [password, setPassword] = useState("Sohail445566")
     const [formData, setFormData] = useState({ username: "", password: "" }) 
     const onChange = (e)=> {
         const {name, value} = e.target
